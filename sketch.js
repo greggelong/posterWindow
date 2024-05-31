f60cdfb0bddf08033784c19e5c62e67ddf2c5f38
Greg K <gkreisman_cep@qq.com>
2024-05-31 12:55:34 +0800

    switch between front and back facing camera

--- sketch.js
+++ sketch.js
@@ -1,8 +1,21 @@
 let capture;
 let wind
 let cx, xy
-
+let lasttouch= 0;
+let frt = true;
 let scl = 1
+
+let constraints = {
+  audio: false,
+  video: {
+    facingMode: {
+      exact: "environment"
+    }
+  }    
+  //video: {
+    //facingMode: "user"
+  //} 
+};
 function preload(){
   wind = loadImage("win1.png")
 }
@@ -10,25 +23,15 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
 
   pixelDensity(1); 
-  var constraints = {
-    audio: false,
-    video: {
-      facingMode: {
-        exact: "environment"
-      }
-    }    
-    //video: {
-      //facingMode: "user"
-    //} 
-  };
-  if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+   
+  //if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
     // true for mobile device
-  capture = createCapture(VIDEO,constraints);
+  //capture = createCapture(VIDEO,constraints);
   //capture = createCapture(VIDEO);
-  }else{
+  //}else{
     // false for not mobile device
    capture = createCapture(VIDEO)
-  }
+ // }
   //
   //capture = createCapture(VIDEO)
   capture.size(width/10,height/5)
@@ -61,8 +64,19 @@ function touchStarted(){
   const timesincelasttouch = currenttime - lasttouch;
 
   if (timesincelasttouch > 500) {
-  rcolumn=floor(random(8))
-  rrow = floor(random(8))
+    if (frt){
+      frt = false
+      capture = createCapture(VIDEO,constraints);
+      capture.size(width/10,height/5)
+      capture.hide();
+      
+    }else{
+      frt = true
+      capture = createCapture(VIDEO);
+      capture.size(width/10,height/5)
+      capture.hide();
+    }
+  
   }
 
   lasttouch = currenttime;
