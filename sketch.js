8d77757e41224c233d0b4a29ad670c25ca9ce530
Greg K <gkreisman_cep@qq.com>
2024-05-31 14:12:17 +0800

    add color chage for testeings

--- sketch.js
+++ sketch.js
@@ -20,7 +20,14 @@ function setup() {
   //capture = createCapture(VIDEO);
   //}else{
     // false for not mobile device
-   capture = createCapture(VIDEO)
+    var constraints = {
+      audio: false,
+           
+      video: {
+        facingMode: "user"
+      } 
+    };
+   capture = createCapture(VIDEO,constraints)
  // }
   //
   //capture = createCapture(VIDEO)
@@ -38,7 +45,11 @@ function setup() {
  
 
  function draw(){
+  if (frt){
   background(60,98,170)
+ }else{
+  background(0)
+ }
   let cnt =windowWidth/2-wind.width/2
   image(capture, cnt+wind.width/5,wind.height/6,wind.width-wind.width/3,wind.height-wind.height/3); 
   filter(POSTERIZE,3)
@@ -66,7 +77,6 @@ function touchStarted(){
           //facingMode: "user"
         //} 
       };
-      
       capture = createCapture(VIDEO,constraints);
       capture.size(width/10,height/5)
       capture.hide();
