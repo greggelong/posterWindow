e7b7340bc8dbb9798724b7d90bcde3cce58e5bef
Greg K <gkreisman_cep@qq.com>
2024-05-31 11:42:10 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+
+Kids: Mom, can we get some AR tonight?
+Mom: No, you have AR at home
+
+This is that AR at home!
---
+++ sketch.js
@@ -0,0 +1,79 @@
+let capture;
+let wind
+let cx, xy
+
+let scl = 1
+function preload(){
+  wind = loadImage("win1.png")
+}
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  pixelDensity(1); 
+  var constraints = {
+    audio: false,
+    video: {
+      facingMode: {
+        exact: "environment"
+      }
+    }    
+    //video: {
+      //facingMode: "user"
+    //} 
+  };
+  //if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+    // true for mobile device
+  //capture = createCapture(VIDEO,constraints);
+  capture = createCapture(VIDEO);
+  //}else{
+    // false for not mobile device
+   //capture = createCapture(VIDEO)
+  //}
+  //
+  //capture = createCapture(VIDEO)
+  capture.size(width/10,height/5)
+  capture.hide();
+  
+  
+   
+  wind.resize(0,windowHeight)
+  //frameRate(1)
+   
+}
+
+
+ 
+
+ function draw(){
+  background(182,208,226)
+  let cnt =windowWidth/2-wind.width/2
+  image(capture, cnt+wind.width/5,wind.height/6,wind.width-wind.width/3,wind.height-wind.height/3); 
+  filter(POSTERIZE,3)
+  
+  image(wind,cnt,0)
+   
+
+}
+
+
+function touchStarted(){
+  const currenttime = millis();
+  const timesincelasttouch = currenttime - lasttouch;
+
+  if (timesincelasttouch > 500) {
+  rcolumn=floor(random(8))
+  rrow = floor(random(8))
+  }
+
+  lasttouch = currenttime;
+  
+}
+
+function mouseClicked(){
+  touchStarted()
+}
+
+
+ 
+ 
+ 
